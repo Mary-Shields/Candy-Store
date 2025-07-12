6eb81cc8d7411da5f33be5bf8546776a33264fbe
mary-shields <maryshields103@gmail.com>
2025-07-12 13:35:16 -0500

    message

---
+++ app/daoCommon.js
@@ -0,0 +1,44 @@
+
+
+const con = require('../config/dbconfig')
+
+const dao = {
+findAll: (res, table)=> {
+con.query(`SELECT * FROM ${table};`,
+            (error, rows)=> {
+                if (!error) {
+                    if (rows.length === 1) {res.json(...rows)
+                    } else {res.json(rows)}
+                } else {
+                    console.log(`Dao Error: ${table}`, error)
+                }
+            }
+        )
+    }, 
+findById: (res, table, id)=> {
+con.query(`SELECT * FROM ${table} Where ${table}_id = ${id};`,
+            (error, rows)=> {
+                if (!error) {
+                    if (rows.length === 1) {res.json(...rows)
+                    } else {res.json(rows)}
+                } else {
+                    console.log(`Dao Error: ${table}`, error)
+                }
+            }
+        )
+    },
+sortGeneral: (res, table)=> {
+con.query(`SELECT * FROM ${table} ORDER BY ${table};`,
+            (error, rows)=> {
+                if (!error) {
+                    if (rows.length === 1) {res.json(...rows)
+                    } else {res.json(rows)}
+                } else {
+                    console.log(`Dao Error: ${table}`, error)
+                }
+            }
+        )
+    }
+}
+
+module.exports = dao
